f22b92235fed9f13dca85124a9b64a95b8aaf8be
Dagoberto <DISATLOZ@hotmail.com>
2021-01-05 20:27:57 -0600

    Net Core site

---
+++ JDSolutionsPOC/Controllers/PricingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JDSolutionsPOC.Controllers
+{
+    public class PricingController : Controller
+    {
+        [HttpGet("modelosdetrabajo")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JDSolutionsPOC/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JDSolutionsPOC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet("productos/pacfe")]
+        public IActionResult PACFE()
+        {
+            return View();
+        }
+        [HttpGet("productos/bluehouse")]
+        public IActionResult BlueHouse()
+        {
+            return View();
+        }
+    }
+}
