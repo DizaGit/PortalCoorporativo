71e4dfb93f829d7835ac67e0587d73291b970a80
Dagoberto <DISATLOZ@hotmail.com>
2021-01-08 09:32:31 -0600

    Actualización

---
+++ JDSolutionsPOC/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using JDInvoiceManager.Helper.CurrentSystem;
+using JDInvoiceManager.Helper.Email;
+using JDSolutionsPOC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JDSolutionsPOC.Controllers
+{
+    public class ContactController : Controller
+    {
+        private IMailSender _sender;
+        public ContactController(IMailSender sender)
+        {
+            _sender = sender;
+        }
+        [HttpGet("contacto")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("contacto/enviar")]
+        public IActionResult SendMail([FromForm]ContactFormData data)
+        {
+            ResponseGeneric r = _sender.SendMail("disa@jdsolutions.com.mx", "Contacto", data.subject, $"{data.email} - {data.message}");
+            return Json(r);
+        }
+        public IActionResult ContactPartial()
+        {
+            return View();
+        }
+    }
+}
---
+++ JDSolutionsPOC/Controllers/ServicesController.cs
@@ -0,0 +1,40 @@
+﻿using JDSolutionsPOC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JDSolutionsPOC.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private IOptionsSnapshot<ServicesInfo> _services { get; set; }
+        public ServicesController(IOptionsSnapshot<ServicesInfo> services)
+        {
+            _services = services;
+        }
+        [HttpGet("servicios")]
+        public IActionResult Index()
+        {
+            return View(_services.Value);
+        }
+        [HttpGet("servicios/grupo/{groupId}")]
+        public IActionResult GroupBy(string groupId)
+        {
+            IEnumerable<Service> services = _services.Value.Services.Where(s => s.groupid == groupId);
+            if (services == null || services.Count() == 0)
+                return NotFound();
+            return View(services);
+        }
+        [HttpGet("servicios/{serviceId}")]
+        public IActionResult Detail(string serviceId)
+        {
+            Service service = _services.Value.Services.FirstOrDefault(s => s.id == serviceId);
+            if (service == null)
+                return NotFound();
+            return View(service);
+        }
+    }
+}
